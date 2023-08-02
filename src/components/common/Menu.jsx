c730d3ee70403e557e1d12f58a31824f7fd07d0b
MauroDS <mauro.diaz.santilli@gmail.com>
2023-08-02 17:02:39 -0300

    Dando forma estructura inicial

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -5,9 +5,9 @@ import { Link, NavLink, useNavigate} from 'react-router-dom';
 const Menu = () => {
   
   return (
-    <Navbar bg="danger" variant="dark" expand="lg">
+    <Navbar bg="primary" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to={'/'}>Cafecito</Navbar.Brand>
+        <Navbar.Brand as={Link} to={'/'}>Mauro Diaz Santilli</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
