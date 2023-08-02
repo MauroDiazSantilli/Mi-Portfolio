59f5f265e171e297dc4347e7b7d8f90abf2a66ca
MauroDS <mauro.diaz.santilli@gmail.com>
2023-08-02 16:27:21 -0300

    Creando estructura de portfolio

---
+++ src/components/common/Menu.jsx
@@ -0,0 +1,23 @@
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { Link, NavLink, useNavigate} from 'react-router-dom';
+
+
+const Menu = () => {
+  
+  return (
+    <Navbar bg="danger" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to={'/'}>Cafecito</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavLink end className='nav-item nav-link' to={'/'}>Inicio</NavLink>
+            <NavLink end className='nav-item nav-link' to={'/registro'}>Registro</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Menu;
