76eacc2490149bf1790fc905dc05016cd7bc98a1
MauroDS <mauro.diaz.santilli@gmail.com>
2023-08-02 18:02:35 -0300

    Instalando nuevas dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Sobre from "./components/views/Sobre";
 import Menu from "./components/common/Menu";
-import Footer from "./components/common/Footer";
-import { BrowserRouter } from "react-router-dom";
+import Contacto from "./components/views/Contacto"
+import Inicio from "./components/views/Inicio"
+import Habilidades from "./components/views/Habilidades"
+import Proyectos from "./components/views/Proyectos";
 
-function App() {
-
+function App() { 
   return (
-    <BrowserRouter>
-    <Menu/>
-    <Footer/>
-    </BrowserRouter>
-  )
+    <div>
+      <Menu />
+      <Inicio />
+      <Sobre />
+      <Habilidades />
+      <Proyectos />
+      <Contacto />
+    </div>
+  );
 }
-
-export default App
+export default App;
---
+++ src/components/views/Sobre.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Sobre() {
+  return (
+    <div>Sobre mi</div> 
+  )
+}
+export default Sobre
